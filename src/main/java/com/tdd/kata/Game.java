35fac187454651fbcdd522ae2b0120520143ee41
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 10:49:30 +0530

    verified if player X is winner if X fills top left to bottom right diagonal

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -38,7 +38,10 @@ public class Game {
         char winner = CHARACTER_NULL;
 
         if (isAnyRowFilledByPlayer(PLAYER_X.value)
-                || isAnyColumnFilledByPlayer(PLAYER_X.value)) {
+                || isAnyColumnFilledByPlayer(PLAYER_X.value)
+                || (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == PLAYER_X.value
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_X.value
+                && getPlayerAt(POSITION_TWO, POSITION_TWO) == PLAYER_X.value)) {
             winner = PLAYER_X.value;
         }
 
--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -188,4 +188,15 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(PLAYER_O.value));
     }
+
+    @Test
+    public void getWinnerShouldReturnXIfXFillsTopLeftToBottomRightDiagonal() {
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_TWO, POSITION_TWO);
+
+        assertThat(game.getWinner(), is(PLAYER_X.value));
+    }
 }
