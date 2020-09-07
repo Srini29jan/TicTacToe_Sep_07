6fa8b9dd3f0c9478aac0fadd4fa8aa1b87f21e7d
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 09:44:49 +0530

    extracted parameter playerO in method isFirstColumnFilledByPlayerO

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -43,17 +43,17 @@ public class Game {
         }
 
         if (isAnyRowFilledByPlayer(PLAYER_O.value)
-                || isFirstColumnFilledByPlayerO()) {
+                || isFirstColumnFilledByPlayerO(PLAYER_O.value)) {
             winner = PLAYER_O.value;
         }
 
         return winner;
     }
 
-    private boolean isFirstColumnFilledByPlayerO() {
-        return getPlayerAt(POSITION_ZERO, POSITION_ZERO) == PLAYER_O.value
-                && getPlayerAt(POSITION_ONE, POSITION_ZERO) == PLAYER_O.value
-                && getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_O.value;
+    private boolean isFirstColumnFilledByPlayerO(char playerO) {
+        return getPlayerAt(POSITION_ZERO, POSITION_ZERO) == playerO
+                && getPlayerAt(POSITION_ONE, POSITION_ZERO) == playerO
+                && getPlayerAt(POSITION_TWO, POSITION_ZERO) == playerO;
     }
 
     private boolean isFirstColumnFilledByPlayerX() {
