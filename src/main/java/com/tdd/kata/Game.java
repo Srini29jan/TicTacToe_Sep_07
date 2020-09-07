e5be89a46352ecb98bd3d1851f785ea6f3cbd63d
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 10:16:33 +0530

    verified if player O is winner if O fills third column

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -46,7 +46,10 @@ public class Game {
 
         if (isAnyRowFilledByPlayer(PLAYER_O.value)
                 || isFirstColumnFilledByPlayer(PLAYER_O.value)
-                || isSecondColumnFilledByPlayer(PLAYER_O.value)) {
+                || isSecondColumnFilledByPlayer(PLAYER_O.value)
+                || (getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_O.value
+                && getPlayerAt(POSITION_ONE, POSITION_TWO) == PLAYER_O.value
+                && getPlayerAt(POSITION_TWO, POSITION_TWO) == PLAYER_O.value)) {
             winner = PLAYER_O.value;
         }
 
--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -176,4 +176,16 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(PLAYER_X.value));
     }
+
+    @Test
+    public void getWinnerShouldReturnOIfOFillsThirdColumn() {
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_TWO, POSITION_TWO);
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_ONE, POSITION_TWO);
+
+        assertThat(game.getWinner(), is(PLAYER_O.value));
+    }
 }
