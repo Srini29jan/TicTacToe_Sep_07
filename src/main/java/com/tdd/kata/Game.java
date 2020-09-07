ad452b6a5c007c00f1333c71105b9277106c88a3
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 11:21:50 +0530

    reused method isTopRightToBottomLeftDiagonalFilledByPlayerO and removed unused method isTopRightToBottomLeftDiagonalFilledByPlayerX

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -40,7 +40,7 @@ public class Game {
         if (isAnyRowFilledByPlayer(PLAYER_X.value)
                 || isAnyColumnFilledByPlayer(PLAYER_X.value)
                 || isTopLeftToBottomRightDiagonalFilledByPlayer(PLAYER_X.value)
-                || isTopRightToBottomLeftDiagonalFilledByPlayerX()) {
+                || isTopRightToBottomLeftDiagonalFilledByPlayerO(PLAYER_X.value)) {
             winner = PLAYER_X.value;
         }
 
@@ -58,12 +58,6 @@ public class Game {
         return getPlayerAt(POSITION_ZERO, POSITION_TWO) == playerO
                 && getPlayerAt(POSITION_ONE, POSITION_ONE) == playerO
                 && getPlayerAt(POSITION_TWO, POSITION_ZERO) == playerO;
-    }
-
-    private boolean isTopRightToBottomLeftDiagonalFilledByPlayerX() {
-        return getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_X.value
-                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_X.value
-                && getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_X.value;
     }
 
     private boolean isTopLeftToBottomRightDiagonalFilledByPlayer(char player) {
