c586ac98bb182d7477393d3a265dd6c43c7a1e60
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 09:50:07 +0530

    extracted method isSecondColumnFilledByPlayerX

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -39,9 +39,7 @@ public class Game {
 
         if (isAnyRowFilledByPlayer(PLAYER_X.value)
                 || isFirstColumnFilledByPlayer(PLAYER_X.value)
-                || (getPlayerAt(POSITION_ZERO, POSITION_ONE) == PLAYER_X.value
-                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_X.value
-                && getPlayerAt(POSITION_TWO, POSITION_ONE) == PLAYER_X.value)) {
+                || isSecondColumnFilledByPlayerX()) {
             winner = PLAYER_X.value;
         }
 
@@ -51,6 +49,12 @@ public class Game {
         }
 
         return winner;
+    }
+
+    private boolean isSecondColumnFilledByPlayerX() {
+        return getPlayerAt(POSITION_ZERO, POSITION_ONE) == PLAYER_X.value
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_X.value
+                && getPlayerAt(POSITION_TWO, POSITION_ONE) == PLAYER_X.value;
     }
 
     private boolean isFirstColumnFilledByPlayer(char player) {
