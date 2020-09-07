f8d1e004da6388120751a7b008c727198dbf8954
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 06:28:04 +0530

    extracted constant POSITION_ZERO

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 
 public class GameTest {
 
+    public static final int POSITION_ZERO = 0;
+
     @Test
     public void getPlayerAtShouldReturnXAfterFirstTurn() {
         Game game = new Game();
 
-        game.playAt(0, 0);
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
 
-        Assert.assertThat(game.getPlayerAt(0, 0), Is.is('X'));
+        Assert.assertThat(game.getPlayerAt(POSITION_ZERO, POSITION_ZERO), Is.is('X'));
     }
 }
