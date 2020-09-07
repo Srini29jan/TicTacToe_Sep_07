1983b435ba04681b3b86b9b312b4e8a01b62587f
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-07 11:17:30 +0530

    verified if player O is winner if O fills top right to bottom left diagonal

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -46,7 +46,10 @@ public class Game {
 
         if (isAnyRowFilledByPlayer(PLAYER_O.value)
                 || isAnyColumnFilledByPlayer(PLAYER_O.value)
-                || isTopLeftToBottomRightDiagonalFilledByPlayer(PLAYER_O.value)) {
+                || isTopLeftToBottomRightDiagonalFilledByPlayer(PLAYER_O.value)
+                || (getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_O.value
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_O.value
+                && getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_O.value)) {
             winner = PLAYER_O.value;
         }
 
--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -222,4 +222,16 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(PLAYER_X.value));
     }
+
+    @Test
+    public void getWinnerShouldReturnOIfOFillsTopRightToBottomLeftDiagonal() {
+        game.playAt(POSITION_ONE, POSITION_TWO);
+        game.playAt(POSITION_TWO, POSITION_ZERO);
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_TWO, POSITION_TWO);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+
+        assertThat(game.getWinner(), is(PLAYER_O.value));
+    }
 }
